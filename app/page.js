790e9e7f3888c7075fc50eaaed52a694fbaab769
Beau Elliott <beauelliott@Beaus-MacBook-Pro.local>
2024-02-20 14:47:53 -0800

    added in hero image

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Nav from "./components/nav/Nav.jsx";
+import Hero from "./components/hero/Hero.jsx";
 
 export default function Home() {
   return (
     <main>
       <Nav />
+      <Hero />
     </main>
   );
 }
